3dfba70a699b5fc2db60b767ea9b6bdb9a99302a
ChrissT75 <christyterence2466@gmail.com>
2023-09-28 16:38:04 -0400

    feat: Implement chat functionality
    - Added chat UI components
    - Implemented real-time messaging

--- app/src/main/java/com/example/blip/models/ChatMessage.java
+++ app/src/main/java/com/example/blip/models/ChatMessage.java
@@ -1,4 +1,14 @@
 package com.example.blip.models;
 
+import java.util.Date;
+
 public class ChatMessage {
+    public  String senderId;
+    public String receiverID;
+
+    public  String message;
+    public String dateTime;
+    public Date dateObject;
+
+
 }
